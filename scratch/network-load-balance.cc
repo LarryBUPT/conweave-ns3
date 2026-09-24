f9effe45619ce09640a1d69ffbc877e020b268bc
LarryBUPT <1196418894@qq.com>
2026-09-24 21:54:02 +0800

    Use measured BDP for imported 400G topology

--- scratch/network-load-balance.cc
+++ scratch/network-load-balance.cc
@@ -1436,7 +1436,7 @@ int main(int argc, char *argv[]) {
     topo2bdpMap[std::string("leaf_spine_128_100G_OS2")] = 104000;  // RTT=8320
     topo2bdpMap[std::string("fat_k8_100G_OS2")] = 156000;      // RTT=12480 --> all 100G links
     topo2bdpMap[std::string("fat_k4_100G_OS2")] = 156000;      // same three-tier link rates
-    topo2bdpMap[std::string("topo_1280_400G_400G_OS1")] = 18000;  // imported MoE topology
+    topo2bdpMap[std::string("topo_1280_400G_400G_OS1")] = 30000;  // 600ns * 400Gbps / 8
 
     // topology_file
     bool found_topo2bdpMap = false;
